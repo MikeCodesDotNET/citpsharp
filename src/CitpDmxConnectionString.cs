7e12c0fc722de5211ebc1f0d7e204609e049cda8
David Butler <david@theimpersonalstereo.com>
2016-02-14 01:59:07 +0000

    Added personalityId field to CitpDmxConnection string. Re-implemented parse using regex

--- src/CitpDmxConnectionString.cs
+++ src/CitpDmxConnectionString.cs
@@ -1,71 +1,99 @@
 ﻿using System;
 using System.Text;
+using System.Text.RegularExpressions;
 using JetBrains.Annotations;
 
 namespace Imp.CitpSharp
 {
+	/// <summary>
+	/// Contains DMX patch information, serializes to a string
+	/// </summary>
 	[PublicAPI]
 	public struct CitpDmxConnectionString : IEquatable<CitpDmxConnectionString>
 	{
+		public const string ProtocolNameArtNet = "ArtNet";
+		public const string ProtocolNameBsre131 = "BSRE1.31";
+		public const string ProtocolNameEtcNet2 = "ETCNet2";
+		public const string ProtocolNameMaNet = "MANet";
+
+		static Regex _dmxConnectionStringRegex = new Regex(@"^(?:(?<p>ArtNet)\/(?<n>\d+)\/(?<u>\d+)\/(?<c>\d+)|(?<p>BSRE1\.31)\/(?<u>\d+)\/(?<c>\d+)|(?<p>ETCNet2)\/(?<c>\d+)|(?<p>MANet)\/(?<t>\d+)\/(?<u>\d+)\/(?<c>\d+))(?:\/PersonalityID\/(?<pId>{[0-9A-Fa-f]{8}-?[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{4}-?[0-9A-Fa-f]{12}}))?$", 
+			RegexOptions.CultureInvariant);
+
+		
+
 		public static CitpDmxConnectionString Parse([NotNull] string s)
 		{
 			if (s == null)
 				throw new ArgumentNullException(nameof(s));
 
-			var tokens = s.Split('/');
+			var match = _dmxConnectionStringRegex.Match(s);
 
-			if (tokens.Length < 2 || tokens.Length > 4)
-				throw new FormatException("Invalid DmxConnectionString");
+			if (!match.Success)
+				throw new FormatException("String is not a valid CITP DmxConnectionString");
 
-			try
+			const int indexProtocol = 1;
+			const int indexNet = 2;
+			const int indexUniverse = 3;
+			const int indexChannel = 4;
+			const int indexType = 5;
+			const int indexPersonalityId = 6;
+
+			Guid? personalityId = null;
+
+			if (match.Groups[indexPersonalityId].Success)
+				personalityId = Guid.Parse(match.Groups[indexPersonalityId].Captures[0].Value);
+
+			switch (match.Groups[indexProtocol].Captures[0].Value)
 			{
-				switch ((DmxProtocol)Enum.Parse(typeof(DmxProtocol), tokens[0]))
-				{
-					case DmxProtocol.ArtNet:
-						return FromArtNet(int.Parse(tokens[1]), int.Parse(tokens[2]), int.Parse(tokens[3]));
+				case ProtocolNameArtNet:
+					return FromArtNet(int.Parse(match.Groups[indexNet].Captures[0].Value), 
+						int.Parse(match.Groups[indexUniverse].Captures[0].Value), 
+						int.Parse(match.Groups[indexChannel].Captures[0].Value), 
+						personalityId);
 
-					case DmxProtocol.Bsre131:
-						return FromBsre131(int.Parse(tokens[1]), int.Parse(tokens[2]));
+				case ProtocolNameBsre131:
+					return FromBsre131(int.Parse(match.Groups[indexUniverse].Captures[0].Value), 
+						int.Parse(match.Groups[indexChannel].Captures[0].Value), 
+						personalityId);
 
-					case DmxProtocol.EtcNet2:
-						return FromEtcNet2(int.Parse(tokens[1]));
+				case ProtocolNameEtcNet2:
+					return FromEtcNet2(int.Parse(match.Groups[indexChannel].Captures[0].Value), 
+						personalityId);
 
-					case DmxProtocol.MaNet:
-						return FromMaNet(int.Parse(tokens[1]), int.Parse(tokens[2]), int.Parse(tokens[3]));
+				case ProtocolNameMaNet:
+					return FromMaNet(int.Parse(match.Groups[indexType].Captures[0].Value),
+						int.Parse(match.Groups[indexUniverse].Captures[0].Value), 
+						int.Parse(match.Groups[indexChannel].Captures[0].Value), 
+						personalityId);
 
-					default:
-						throw new FormatException("Invalid DmxConnectionString");
-				}
+				default:
+					throw new FormatException("String is not a valid CITP DmxConnectionString");
 			}
-			catch (FormatException ex)
-			{
-				throw new InvalidOperationException("Invalid DmxConnectionString", ex);
-			}
 		}
 
-		public static CitpDmxConnectionString FromArtNet(int net, int universe, int channel)
+		public static CitpDmxConnectionString FromArtNet(int net, int universe, int channel, Guid? personalityId = null)
 		{
-			return new CitpDmxConnectionString(DmxProtocol.ArtNet, net, universe, channel, 0);
+			return new CitpDmxConnectionString(DmxProtocol.ArtNet, channel, net, universe, null, personalityId);
 		}
 
-		public static CitpDmxConnectionString FromBsre131(int universe, int channel)
+		public static CitpDmxConnectionString FromBsre131(int universe, int channel, Guid? personalityId = null)
 		{
-			return new CitpDmxConnectionString(DmxProtocol.Bsre131, 0, universe, channel, 0);
+			return new CitpDmxConnectionString(DmxProtocol.Bsre131, channel, null, universe, null, personalityId);
 		}
 
-		public static CitpDmxConnectionString FromEtcNet2(int channel)
+		public static CitpDmxConnectionString FromEtcNet2(int channel, Guid? personalityId = null)
 		{
-			return new CitpDmxConnectionString(DmxProtocol.Bsre131, 0, 0, channel, 0);
+			return new CitpDmxConnectionString(DmxProtocol.EtcNet2, channel, null, null, null, personalityId);
 		}
 
-		public static CitpDmxConnectionString FromMaNet(int type, int universe, int channel)
+		public static CitpDmxConnectionString FromMaNet(int type, int universe, int channel, Guid? personalityId = null)
 		{
-			return new CitpDmxConnectionString(DmxProtocol.Bsre131, 0, universe, channel, type);
+			return new CitpDmxConnectionString(DmxProtocol.MaNet, channel, null, universe, type, personalityId);
 		}
 
 
 
-		CitpDmxConnectionString(DmxProtocol protocol, int net, int universe, int channel, int type)
+		CitpDmxConnectionString(DmxProtocol protocol, int channel, int? net, int? universe, int? type, Guid? personalityId)
 			: this()
 		{
 			Protocol = protocol;
@@ -73,6 +101,7 @@ namespace Imp.CitpSharp
 			Universe = universe;
 			Channel = channel;
 			Type = type;
+			PersonalityId = personalityId;
 		}
 
 
@@ -89,25 +118,29 @@ namespace Imp.CitpSharp
 
 		public DmxProtocol Protocol { get; }
 
-		public int Net { get; }
-		public int Type { get; }
+		public int? Net { get; }
+		public int? Type { get; }
 
-		public int Universe { get; }
+		public int? Universe { get; }
 		public int Channel { get; }
+
+		public Guid? PersonalityId { get; }
 
 
 		public override string ToString()
 		{
+			string personalityId = PersonalityId.HasValue ? "/PersonalityID/" + PersonalityId.Value.ToString("B") : string.Empty;
+
 			switch (Protocol)
 			{
 				case DmxProtocol.ArtNet:
-					return $"ArtNet/{Net}/{Universe}/{Channel}";
+					return $"{ProtocolNameArtNet}/{Net}/{Universe}/{Channel}{personalityId}";
 				case DmxProtocol.Bsre131:
-					return $"BSRE1.31/{Universe}/{Channel}";
+					return $"{ProtocolNameBsre131}/{Universe}/{Channel}{personalityId}";
 				case DmxProtocol.EtcNet2:
-					return $"ETCNet2/{Channel}";
+					return $"{ProtocolNameEtcNet2}/{Channel}{personalityId}";
 				case DmxProtocol.MaNet:
-					return $"MANet/{Type}/{Universe}/{Channel}";
+					return $"{ProtocolNameMaNet}/{Type}/{Universe}/{Channel}{personalityId}";
 				default:
 					return string.Empty;
 			}
@@ -120,8 +153,7 @@ namespace Imp.CitpSharp
 
 		public bool Equals(CitpDmxConnectionString other)
 		{
-			return Protocol == other.Protocol && Net == other.Net && Type == other.Type && Universe == other.Universe
-			       && Channel == other.Channel;
+			return Protocol == other.Protocol && Net == other.Net && Type == other.Type && Universe == other.Universe && Channel == other.Channel && PersonalityId.Equals(other.PersonalityId);
 		}
 
 		public override bool Equals([CanBeNull] object obj)
@@ -136,10 +168,11 @@ namespace Imp.CitpSharp
 			unchecked
 			{
 				int hashCode = (int)Protocol;
-				hashCode = (hashCode * 397) ^ Net;
-				hashCode = (hashCode * 397) ^ Type;
-				hashCode = (hashCode * 397) ^ Universe;
+				hashCode = (hashCode * 397) ^ Net.GetHashCode();
+				hashCode = (hashCode * 397) ^ Type.GetHashCode();
+				hashCode = (hashCode * 397) ^ Universe.GetHashCode();
 				hashCode = (hashCode * 397) ^ Channel;
+				hashCode = (hashCode * 397) ^ PersonalityId.GetHashCode();
 				return hashCode;
 			}
 		}
